fe76dea7b292abcdc42d0e65e96fc0449eb372ef
Rauru <raul_lopez_tu@hotmail.com>
2017-06-22 17:47:53 -0600

    commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <map>
 using namespace std;
 
+map<int, map<int,int> > memo;
+
 int longestPalindromicSubsequence(string sequence, int begin, int end)
 {
    if (begin == end)
@@ -11,17 +13,26 @@ int longestPalindromicSubsequence(string sequence, int begin, int end)
    if (sequence[begin] == sequence[end] && begin + 1 == end)
      return 2;
 
-   if (sequence[begin] == sequence[end])
-      return longestPalindromicSubsequence(sequence, begin+1, end-1) + 2;
-
-   return max( longestPalindromicSubsequence(sequence, begin, end-1),
+   if(memo.find(begin) == memo.end() || memo[begin].find(end) == memo[begin].end())
+   {
+    if (sequence[begin] == sequence[end])
+    {
+      memo[begin][end] = longestPalindromicSubsequence(sequence, begin+1, end-1) + 2;
+    }else{
+      return max( longestPalindromicSubsequence(sequence, begin, end-1),
            longestPalindromicSubsequence(sequence, begin+1, end) );
+    }
+   }
+
+   return memo[begin][end];
 }
 
 int longestPalindromicSubsequence(string sequence)
 {
+  memo.clear();
   return longestPalindromicSubsequence(sequence, 0, sequence.size()-1);
 }
+
 void findPath(int **DAG,int size,int source,int destination,vector<int> *answer)
 {
   int next = 0,nextWeight=9999;
@@ -57,6 +68,7 @@ vector<int> getPath(int **DAG, int size, int source, int destination)
   }
   return answer;
 }
+
 
 int main ()
 {
